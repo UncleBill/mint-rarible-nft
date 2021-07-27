2e1b1fbc6f2fa9294ce40946e0f9d4fc505f4417
unclebill <billbill290@gmail.com>
2021-07-27 11:47:25 +0800

    use rarepress.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,37 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useRef, useState, InputHTMLAttributes } from "react";
+import "rarepress.js";
+// rarepress.js@0.0.14 can not run in strict mode
+// import rarepress from "rarepress.js";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const ref = useRef(null);
+  const [result, setResult] = useState("");
+  const handleChange: InputHTMLAttributes<HTMLInputElement>["onChange"] =
+    async (e) => {
+      const file = e.target.files![0];
+      mint(file);
+    };
 
+  const mint = async (file: File) => {
+    await rarepress.init({ host: "https://rinkeby.rarepress.org/v0" });
+    let cid = await rarepress.add(file);
+    let token = await rarepress.create({
+      metadata: {
+        name: "Hello World NFT",
+        description: "Minting NFT so easy!",
+        image: "/ipfs/" + cid,
+      },
+    });
+    setResult(JSON.stringify(token, null, 2));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <input ref={ref} onChange={handleChange} type="file" name="file" />
+      <pre>{result}</pre>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  plugins: [reactRefresh()],
+  server: {
+    proxy: {
+      "/upload": {
+        target: "https://pinata.rarible.com/upload",
+      },
+    },
+  },
+});
